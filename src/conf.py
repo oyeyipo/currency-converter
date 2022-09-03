0e25a81ecdbee804ef8bf5164fd5b2824d5f862f
Olawale Oyeyipo <olaclassik@gmail.com>
2022-09-03 10:49:28 +0100

    change style extension

--- src/conf.py
+++ src/conf.py
@@ -5,6 +5,6 @@ BASE_DIR = pathlib.Path(__file__).parent.parent
 
 ASSETS_DIR = BASE_DIR.joinpath("assets")
 
-CSS_FILE = ASSETS_DIR / "css/styles.css"
+CSS_FILE = ASSETS_DIR / "css/styles.qss"
 
 NO_MARGIN = QMargins(0, 0, 0, 0)
