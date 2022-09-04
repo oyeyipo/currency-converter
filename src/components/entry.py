1f52ff73a0036f91b79b358fd54a01275b6801e2
Olawale Oyeyipo <olaclassik@gmail.com>
2022-09-04 12:06:49 +0100

    amount collector now functional

--- src/components/entry.py
+++ src/components/entry.py
@@ -1,15 +1,18 @@
-import base64
+from base64 import b64decode
 from typing import List
-from typing_extensions import Self
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QIcon, QPixmap, QDoubleValidator
 from PySide6.QtWidgets import (
-    QWidget,
-    QDoubleSpinBox,
     QComboBox,
-    QHBoxLayout,
+    QFormLayout,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
     QSizePolicy,
+    QWidget,
 )
-from PySide6.QtGui import QIcon, QPixmap
-from PySide6.QtCore import QSize
+from typing_extensions import Self
 from utilities.loader import get_pppdata
 
 HIEGHT = 45
@@ -24,36 +27,82 @@ class EntryWidget(QWidget):
         self.initializeUI()
 
     def initializeUI(self):
+        layout = QVBoxLayout(self)
 
-        layout = QHBoxLayout(self)
+        selector_sp = QSizePolicy()
+        selector_sp.setHorizontalPolicy(QSizePolicy.Maximum)
+        selector_sp.setVerticalPolicy(QSizePolicy.Maximum)
+        selector_sp.setHorizontalStretch(0)
 
-        self.amount = QDoubleSpinBox(self)
+        amount_sp = QSizePolicy()
+        amount_sp.setVerticalPolicy(QSizePolicy.Maximum)
+        amount_sp.setVerticalStretch(0)
+
+        input_form = QFormLayout()
+        input_form.setFieldGrowthPolicy(
+            input_form.FieldGrowthPolicy.AllNonFixedFieldsGrow
+        )
+        # input_form.setFormAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        # input_form.setLabelAlignment(Qt.AlignLeft)
+        input_form.setAlignment(Qt.AlignVCenter)
+        input_form.setSpacing(10)
+
+        self.currency_symbol = QLabel()
+        self.currency_symbol.setObjectName("symbol")
+
+        self.amount = QLineEdit(self)
         self.amount.setObjectName("valueInput")
-        self.amount.setButtonSymbols(QDoubleSpinBox.NoButtons)
+        self.amount.setPlaceholderText("0")
+        validator = QDoubleValidator()
+        validator.setDecimals(2)
+        self.amount.setValidator(validator)
+        # self.amount.setSizePolicy(amount_sp)
+        self.amount.textEdited.connect(self.amount_changed)
+
+        input_form.addRow(self.currency_symbol, self.amount)
 
         self.selector = QComboBox(self)
         self.selector.setObjectName("chooser")
-
-        self.add_countries(self.selector)
+        self.selector.setSizePolicy(selector_sp)
         self.selector.setMaxVisibleItems(15)
+        self.selector.currentTextChanged.connect(self.selector_text_changed)
 
-        sp = QSizePolicy()
-        sp.setHorizontalPolicy(QSizePolicy.Maximum)
-        self.selector.setSizePolicy(sp)
+        self.set_countries()
+        self.set_currency_symbol()
 
-        layout.addWidget(self.selector)
-        layout.addWidget(self.amount)
+        layout.addLayout(input_form)
+        layout.addWidget(self.selector, stretch=0, alignment=Qt.AlignLeft)
 
-    def add_countries(self: Self, selector: QComboBox) -> None:
-        countries = get_pppdata()
-        for country_name, data in countries.items():
-            image_blob = base64.b64decode(data["flag"])
-            image = QPixmap()
-            image.loadFromData(image_blob)
-            icon = QIcon(image)
-            selector.addItem(
+    def set_countries(self: Self) -> None:
+        self.countries = get_pppdata()
+        for country_name, data in self.countries.items():
+            image_blob = b64decode(data["flag"])
+            icon = QIcon(self.get_image_from_blob(image_blob))
+            self.selector.addItem(
                 icon,
                 "{name} - {currency}".format(
                     name=country_name, currency=data["currency"]["name"]
                 ),
             )
+
+    def get_image_from_blob(self: Self, blob: bytes) -> QPixmap:
+        image = QPixmap()
+        image.loadFromData(blob)
+        return image
+
+    def set_currency_symbol(self):
+        country_name, _ = self.selector.currentText().split(" - ")
+        country_symbol = self.countries[country_name]["currency"]["symbol"]
+        if not country_symbol:
+            country_symbol = self.countries[country_name]["currency"]["code"]
+        self.currency_symbol.setText(country_symbol)
+
+    def selector_text_changed(self, text):
+        self.set_currency_symbol()
+
+    def amount_changed(self, text: str):
+        main_amount, *fractional_amount = text.split(".")
+        main_amount = main_amount.replace(",", "")
+        main_amount = "{:,d}".format(int(main_amount)) if main_amount else ""
+        amount = ".".join([main_amount] + fractional_amount)
+        self.amount.setText(amount)
